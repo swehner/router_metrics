0ed9cbd7b4dc8b71613c50954bdbbf3fecf94cc4
Stefan Wehner <stefan.wehner@gmail.com>
2017-10-29 15:54:57 +0100

    Refactor

---
+++ get_metrics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ENDPOINT=$1
+AUTH=$2
+COLUMNS=$3
+TAGS=$4
+
+curl -s -u $AUTH $ENDPOINT \
+	| tidy -q -asxml -n 2>/dev/null \
+	| xpath -n -q -e '//tr[count(td)='$COLUMNS']/td/text()' \
+	| python parse_metrics.py $COLUMNS $TAGS
+
--- get_wan_metrics.sh
+++ get_wan_metrics.sh
@@ -3,8 +3,5 @@
 AUTH=$1
 COLUMNS=10
 
-curl -s -u $AUTH 192.168.1.1/statswan.cmd \
-	| tidy -q -asxml -n 2>/dev/null \
-	| xpath -q -e '//tr[count(td)='$COLUMNS']/td/text()' \
-       	| python parse_metrics.py $COLUMNS device=comtrend,type=wan
+bash -x `dirname $0`/get_metrics.sh 192.168.1.1/statswan.cmd $AUTH $COLUMNS device=comtrend,type=wan
 
