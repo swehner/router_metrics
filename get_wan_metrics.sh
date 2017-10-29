2a0f0a8edb6d8b178588674f79a60b6efa36262f
Stefan Wehner <stefan.wehner@gmail.com>
2017-10-29 16:02:25 +0100

    Remove -x

--- get_wan_metrics.sh
+++ get_wan_metrics.sh
@@ -3,5 +3,5 @@
 AUTH=$1
 COLUMNS=10
 
-bash -x `dirname $0`/get_metrics.sh 192.168.1.1/statswan.cmd $AUTH $COLUMNS device=comtrend,type=wan
+`dirname $0`/get_metrics.sh 192.168.1.1/statswan.cmd $AUTH $COLUMNS device=comtrend,type=wan
 
